d20293d1376ad658de944392adf660e3b185fb28
vitaliikobrin <vitaliy.kobrin@gmail.com>
2020-05-25 00:51:51 +0300

    Add ability to run DB backup by custom schedule

--- run-arangodb.sh
+++ run-arangodb.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-echo
-echo Create volume for DB
-docker volume create ArangoDB_Data
-echo Volume created
+db_image=vitaliikobrin/arangodb
+data_volume=ArangoDB_Data
+db_container_name=test-arangodb
 
 echo
-echo Run DB container based on vitaliikobrin/arangodb image
+echo Create volume for DB
+docker volume create $data_volume
+
+echo
+echo Create network
+docker network create app_network
+
+echo
+echo Run DB container based on $db_image image
 docker run -d \
 	--name $db_container_name \
 	--network=app_network \
-	-v ArangoDB_Data:/var/lib/arangodb3 \
+	-v $data_volume:/var/lib/arangodb3 \
 	-v "$(pwd)"/backups:/artifacts \
 	-p 8529:8529 \
 	--env-file arangodb-properties.env \
 	--ulimit nofile=65535:65535 \
-	vitaliikobrin/arangodb
+	$db_image
---
+++ scripts/backup-db.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+backup_prefix="${ARANGO_DB_NAME}-${STAGE}-backup"
+s3_backup_dir=${ARANGO_S3_BACKUP_DIR}
+
+current_date_time=$(date '+%Y-%m-%d-%H-%M-%S')
+backup_name=$backup_prefix-$current_date_time.tar.gz
+
+mkdir -p artifacts && cd artifacts
+
+print_log () {
+	echo
+	echo $(date '+%Y-%m-%dT%H:%M:%SZ') "|" $1
+}
+
+print_log "Prepare $ARANGO_DB_NAME $STAGE database backup"
+arangodump \
+	--server.username ${ARANGO_USER} \
+	--server.password ${ARANGO_ROOT_PASSWORD} \
+	--server.database ${ARANGO_DB_NAME} \
+	--output-directory ${backup_prefix} \
+	--compress-output
+
+print_log "Compress $ARANGO_DB_NAME $STAGE database backup"
+tar -zcvf $backup_name $backup_prefix/
+
+print_log "Upload backup to S3"
+aws s3 cp $backup_name s3://$s3_backup_dir/
+
+print_log "Remove $ARANGO_DB_NAME $STAGE database backup artifacts"
+rm -rf $backup_prefix
+
+print_log "Done!"
