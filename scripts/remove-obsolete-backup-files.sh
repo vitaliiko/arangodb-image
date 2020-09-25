ae8eb60b589c4b291dcf99af52d8d1d2891cfce5
vitaliikobrin <vitaliy.kobrin@gmail.com>
2020-09-25 13:03:51 +0300

    Add script to remove obsolete backup files

---
+++ scripts/remove-obsolete-backup-files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+backup_prefix="${ARANGO_DB_NAME}-${STAGE}-backup"
+s3_backup_dir=${ARANGO_S3_BACKUP_DIR}
+
+files_list=$(aws s3 ls s3://${s3_backup_dir}/ | grep ${backup_prefix})
+backup_files_count=$(echo "$files_list" | wc -l)
+
+if [ "$backup_files_count" -gt "$ARANGO_BACKUP_FILES_COUNT" ]
+then
+    files_to_remove=$(echo "$files_list" \
+        | head -n -$ARANGO_BACKUP_FILES_COUNT \
+        | awk -v backup_path="$s3_backup_dir" '{print "s3://"backup_path"/"$4}')
+
+    echo "Removing obsolete backup files"
+    echo $files_to_remove | xargs -n1 aws s3 rm
+fi
