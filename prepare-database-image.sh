b5df1f454b5727d8e829ad7defe6fa15665b6204
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2025-09-08 12:01:57 +0300

    Update ARM image build

--- prepare-database-image.sh
+++ prepare-database-image.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 x86_image_name=vitaliikobrin/arangodb:2.2.3
-arm_image_name=vitaliikobrin/arangodb:2.2.3-arm
+arm_image_name=vitaliikobrin/arangodb:2.2.4-arm
 
 docker build -t $x86_image_name .
 docker push $x86_image_name
 
-docker build -t $arm_image_name .
+docker build -t $arm_image_name -f Dockerfile.arm .
 docker push $arm_image_name
